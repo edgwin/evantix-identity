f84cdb9cd801306b4906a883e19b3178b561da4b
Edgwin Holguin <edgwin@hotmail.com>
2021-08-17 17:46:11 -0500

    Cambio el default connection string

--- api/Migrations/20210810183446_Init.Designer.cs
+++ api/Migrations/20210810183446_Init.Designer.cs
@@ -10,14 +10,14 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IdentityService.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    [Migration("20201020184911_Init")]
+    [Migration("20210810183446_Init")]
     partial class Init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.6-servicing-10079")
+                .HasAnnotation("ProductVersion", "2.2.0-rtm-35687")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
